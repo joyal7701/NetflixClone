c6214477379cc8c901bb34d0c4bb1f168e6bd2ea
Joyal Shah <63547847+joyal7701@users.noreply.github.com>
2021-07-29 19:05:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyCOfhB5v8UBBbKFVPxf6XY2dvBGqOHLD-w",
-  authDomain: "netflixclone-49ead.firebaseapp.com",
-  projectId: "netflixclone-49ead",
-  storageBucket: "netflixclone-49ead.appspot.com",
-  messagingSenderId: "549849026965",
-  appId: "1:549849026965:web:713780f827794e5bea269c",
+  apiKey: "AIzaSyBZR7ncs2tIti5yzaq4T-7QzZiiV9_aUmk",
+  authDomain: "netflixclone-d83d9.firebaseapp.com",
+  projectId: "netflixclone-d83d9",
+  storageBucket: "netflixclone-d83d9.appspot.com",
+  messagingSenderId: "291708332176",
+  appId: "1:291708332176:web:c29b650310ddf912bad015",
+  measurementId: "G-VVNV4HCHH5"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
